359e58fab80db296ed39112d72c03f4542aae2b5
Winston Castillo <w2casti@gmail.com>
2020-06-01 22:43:14 +0800

    Revert "Add call to disk_full function"
    Reason for rollback: the disk_full is calling a not defined function
    
    This reverts commit bb14e04db4e1f476af08daa72fb643b5fe9cb05d.

--- scripts/all_checkes.py
+++ scripts/all_checkes.py
@@ -11,9 +11,6 @@ def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
-    if disk_full():
-        print("Disk Full.")
-        sys.exit(1)
     print("Everything ok.")
     sys.exit(0)
 
