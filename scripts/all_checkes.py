e4c7036a5e1ebd8063edb9db561375437f56dbe3
Winston Castillo <w2casti@gmail.com>
2020-06-01 21:53:44 +0800

    Call check_reboot from main, sys.exit(1)

---
+++ scripts/all_checkes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+def check_reboot():
+    """Return True if the computer has a pending reboot."""
+    return os.path.exist("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+
+main()
